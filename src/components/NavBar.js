8397586e329566b9de30e0877b04b8d5b05ed4e0
Vaibhav Kaushik <68017022+vansul@users.noreply.github.com>
2020-10-30 03:45:35 +0530

    Reverted back.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ export default class NavBar extends Component {
       loggedIn: sessionStorage.getItem('loggedIn'),
       uname: sessionStorage.getItem('uname'),
     };
-    values.update = this.forceUpdate;
+    values.update = this.forceUpdate.bind(this);
   }
 
   render() {
